05de791f599a36bedb7e45fa873bef24c66a8a29
lejinvarghese <lejinsnests@yahoo.com>
2021-07-31 15:57:08 -0400

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Fun with Graphs
+# Graph Theory and Network Science
 
 Graph algorithms, projects and utils to come.
+
+![image](docs/sample.png)
--- utils.py
+++ utils.py
@@ -12,15 +12,18 @@ from tqdm.notebook import trange, tqdm
 from IPython.display import display, HTML
 
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 from pylab import cycler
+
+from scipy.spatial.distance import cosine, pdist, squareform
+from scipy.stats import stats
+import statsmodels.api as sm
 
 warnings.filterwarnings("ignore")
 plt.rcParams['font.family'] = 'sans-serif'
-plt.rcParams['font.sans-serif'] = 'Adobe Clean'
 plt.rcParams['axes.prop_cycle'] = cycler(color=['r', 'g', 'b', 'y'])
 pd.set_option('display.max_columns', None)
 
-_path = os.getcwd()
-DIRECTORY = str(Path(_path).parents[2])
-PROJECT = str(Path(_path).parents[1]).split('/')[-1]
+PATH = os.getcwd()
+DIRECTORY = str(Path(PATH).parents[2])
+PROJECT = str(Path(PATH).parents[1]).split('/')[-1]
