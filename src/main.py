a7693549073210588712c2a9474f2437743234da
lejinvarghese <lejinsnests@gmail.com>
2022-05-25 21:19:10 -0400

    hotfix wrote measures out

--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ TWITTER_USERNAME = os.getenv("TWITTER_USERNAME")
 TWITTER_API_BEARER_TOKEN = os.getenv("TWITTER_API_BEARER_TOKEN")
 INTEGRATED_FOCAL_NODE_ID = os.getenv("INTEGRATED_FOCAL_NODE_ID")
 CLOUD_STORAGE_BUCKET = os.getenv("CLOUD_STORAGE_BUCKET")
+MAX_RADIUS = 2
 
 
 def main():
@@ -28,14 +29,22 @@ def main():
     # twitter_hood.update_neighborhood()
     network = HomogenousEgoNetwork(
         focal_node_id=INTEGRATED_FOCAL_NODE_ID,
-        radius=2,
+        radius=MAX_RADIUS,
         storage_bucket=CLOUD_STORAGE_BUCKET,
     )
     measures = network.create_measures(
         calculate_nodes=True, calculate_edges=True
     )
-    print(measures.summary_measures)
-    print(measures.node_measures)
+
+    measures.summary_measures.to_csv(
+        f"{CLOUD_STORAGE_BUCKET}/data/processed/measures/{MAX_RADIUS}/summary_measures.csv",
+        index=False,
+    )
+
+    measures.node_measures.to_csv(
+        f"{CLOUD_STORAGE_BUCKET}/data/processed/measures/{MAX_RADIUS}/node_measures.csv",
+        index=False,
+    )
 
 
 if __name__ == "__main__":
