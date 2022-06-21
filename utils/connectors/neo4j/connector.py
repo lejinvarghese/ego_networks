3b732e57425ed91c1d720a36b8c70bb04558990d
lejinvarghese <lejin.varghese@shopify.com>
2022-06-20 21:56:51 -0400

    node and edge writes work

--- utils/connectors/neo4j/connector.py
+++ utils/connectors/neo4j/connector.py
@@ -11,6 +11,10 @@ NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
 
 
 class Neo4jConnector:
+    """
+    This connector uses direct Cypher queries though there are helper functions for this library being used.
+    """
+
     def __init__(self, profile, user, password):
         self.graph = py2neo.Graph(profile, auth=(user, password))
         self.test_connection()
@@ -27,11 +31,60 @@ class Neo4jConnector:
         except:
             print("Connection to Neo4j unsuccessful")
 
+    def empty_database(self):
+        self.graph.delete_all()
 
-if __name__ == "__main__":
+    def create_nodes(self, nodes=None, entity_type="entity", batch_size=500):
+        query = f"""
+        UNWIND $node_list as node
+        CREATE (e: {entity_type} {{
+            id: node.id,
+            name: node.name,
+            commercial_region_code: node.commercial_region_code
+        }})
+        """
+        print(query)
+
+        for b_start in range(0, len(nodes), batch_size):
+            b_end = b_start + batch_size
+            records = nodes.iloc[b_start:b_end].to_dict("records")
+            tx = self.graph.auto()
+            tx.evaluate(query, parameters={"node_list": records})
+
+    def create_relationships(
+        self,
+        edges=None,
+        entity_type="entity",
+        relationship_type="connected_to",
+        batch_size=500,
+    ):
+        query = f"""
+        UNWIND $edge_list as edge
+        MATCH (source: {entity_type} {{id: edge.source}})
+        MATCH (target: {entity_type} {{id: edge.target}})
+
+        MERGE (source)-[r: {relationship_type}] -> (target)
+
+        SET r.weight = toInteger(edge.weight)
+        """
+        print(query)
+
+        for b_start in range(0, len(edges), batch_size):
+            b_end = b_start + batch_size
+            records = edges.iloc[b_start:b_end].to_dict("records")
+            tx = self.graph.auto()
+            tx.evaluate(query, parameters={"edge_list": records})
+
+
+def main():
     nodes = pd.read_csv("data/nodes.csv")
     edges = pd.read_csv("data/edges.csv")
+    neo4j_connector = Neo4jConnector(NEO4J_HOST, NEO4J_USER, NEO4J_PASSWORD)
+    neo4j_connector.empty_database()
+    neo4j_connector.create_nodes(nodes=nodes, entity_type="merchants")
+    neo4j_connector.create_relationships(edges=edges, entity_type="merchants")
 
-    neo4j_connector = Neo4jConnector(
-        profile=NEO4J_HOST, user=NEO4J_USER, password=NEO4J_PASSWORD
-    )
+
+if __name__ == "__main__":
+
+    main()
