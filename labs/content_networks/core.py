1da9ce742ddf0400fe2233f8c6ba342c78dee59e
lejinvarghese <lejinsnests@gmail.com>
2023-12-26 18:56:38 -0500

    changed abstract
    Signed-off-by: lejinvarghese <lejinsnests@gmail.com>

--- labs/content_networks/core.py
+++ labs/content_networks/core.py
@@ -1,14 +1,16 @@
 """
 Core abstract functionalities.
 """
-from abc import ABC, property
+from abc import ABC, abstractproperty
 from enum import Enum
 from typing import List, Float, String
 
+
 class Meme(ABC):
-    @property
+    @abstractproperty
     def summary(self) -> String:
         pass
+
 
 class MemeticExpression(Enum):
     explicit = 0
@@ -17,35 +19,36 @@ class MemeticExpression(Enum):
     post_ironic = 3
     meta_ironic = 4
 
+
 class Idea:
-    def __init__(self, memes: List[Meme], expressions:List[MemeticExpression], strengths: List[Float]):
+    def __init__(
+        self,
+        memes: List[Meme],
+        expressions: List[MemeticExpression],
+        strengths: List[Float],
+    ):
         self.memes = memes
         self.expressions = expressions
         self.strengths = strengths
 
+
 class Content(ABC):
-    
-    @property
+    @abstractproperty
     def id(self) -> String:
         pass
-    
-    @property
+
+    @abstractproperty
     def name(self) -> String:
         pass
 
-    @property
+    @abstractproperty
     def summary(self) -> String:
         pass
-    
-    @property
+
+    @abstractproperty
     def format(self) -> String:
         pass
-    
-    @property
+
+    @abstractproperty
     def ideas(self) -> List[Idea]:
         pass
-    
-
-    
-    
-    
